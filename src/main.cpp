0f54fae1c9de29eeaebbbb4bcc68eeec804e544b
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2025-06-05 12:26:13 +0200

    Add test codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,39 @@
 #include <Arduino.h>
 #include <ThermalPrinter.h>
-#include <esp32-hal-log.h>
 
 #include "images.h"
 
-ThermalPrinter printer(Serial2, 25, 26);
+ThermalPrinter printer(Serial1);
 
 void setup() {
     Serial.begin(115200);
-    // Serial2.begin(115200, SERIAL_8N1, 25, 26);
-    // log_i("Booting up");
 
-    // Serial.onReceive([](){
-    //     size_t available = Serial.available();
-    //     while(available--)
-    //         Serial2.write(Serial.read());
-    // }, true);
-    // Serial2.onReceive([](){
-    //     size_t available = Serial2.available();
-    //     while(available--)
-    //         Serial.write(Serial2.read());
-    // }, true);
-
-    delay(5000);
+    Serial1.setPinout(5,6);
+    Serial1.setCTS(7);
+    Serial1.setRTS(8);
+    Serial1.begin();
 
     printer.begin();
 
     printer.println("Hello World");
-    // // // printer.printBinaryFile(aws_root_ca_pem_start, aws_root_ca_pem_end-aws_root_ca_pem_start);
+    // printer.printBinaryFile(aws_root_ca_pem_start, aws_root_ca_pem_end-aws_root_ca_pem_start);
     printer.printBarcode("123ABC", ThermalPrinter::BarcodeType::CODE39);
     printer.feed(3);
 
     printer.printQrCode("Hello World", 8);
 
-    printer.printTiff(test_tiff);
+    printer.printTiff(test_png);
 
-    // printer.println("Font test:");
-    // uint8_t i = 4;
-    // printer.setFont(i);
-    // delay(100);
-    // printer.printf("\tFont %d\n", i);
-    // for(char c = 0; c < 255; c++) {
-    //     printer.print(c);
-    // }
-    // printer.println();
-    // delay(100);
+    printer.println("Font test:");
+    uint8_t i = 4;
+    printer.setFont(i);
+    delay(100);
+    printer.printf("\tFont %d\n", i);
+    for(char c = 0; c < 255; c++) {
+        printer.print(c);
+    }
+    printer.println();
+    delay(100);
 }
 
 void loop() {
@@ -52,11 +41,11 @@ void loop() {
     size_t count = Serial.available();
     while(count--){
         const char c = Serial.read();
-        Serial2.print(c);
+        Serial1.print(c);
     }
-    count = Serial2.available();
+    count = Serial1.available();
     while(count--) {
-        const char c = Serial2.read();
+        const char c = Serial1.read();
         Serial.print(c);
     }
 
